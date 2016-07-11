87cf382d3802f46f5a9841483d07b476324296ee
Nic Higuera <damagecpl@gmail.com>
2016-07-11 11:14:48 -0700

    updated app.js

--- app.js
+++ app.js
@@ -18,3 +18,62 @@ let matchupJson = require('./routes/matchup_json')
 let apiStatic = require('./routes/api_static')
 let stats = require('./routes/stats')
 let index = require('./routes/index')
+
+//view engine
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(compress());
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(morgan('dev'));
+app.use(bodyParser.json({limit: '2kb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '2kb', extended: true}));
+
+app.use(express.static(path.join(__dirname, 'public'), {maxAge:86400000}));
+
+
+
+
+app.use(overallData);
+
+app.use('/champion', champion);
+app.use('/matchup', matchup);
+app.use('/matchupJson', matchupJson);
+app.use('/static', apiStatic);
+
+app.use('/statistics', statistics);
+app.use('/faq', faq);
+app.use('/', index);
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+      res.statusCode = err.status;
+      res.render('error', {
+          pageData:{
+            appName: 'core',
+            name:'error',
+            title: 'We got ourselves a problem...'
+          },
+          message: err.message,
+          error: err
+      });
+  });
+} else {
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.statusCode = err.status;
+    res.render('error', {
+        pageData:{
+          appName: 'core',
+          name:'error',
+          title: 'We got ourselves a wild teemo problem...'
+        },
+        message: err.message,
+        error: {}
+    });
+  });
+}
+
+module.exports = app;
