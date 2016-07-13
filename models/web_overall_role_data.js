054cc41419cec93774f59fbd95833fd07c1c2aa9
Nic Higuera <damagecpl@gmail.com>
2016-07-13 13:28:45 -0700

    added web overall role data schema

---
+++ models/web_overall_role_data.js
@@ -0,0 +1,10 @@
+var mongoose = requiure('mongoose')
+
+var webOverallRoleData = new mongoose.Schema({
+	role: String,
+	totalNumber: Number,
+	matrixLabels: [String],
+	patchPlay: [Number]
+});
+
+module.exports = mongoose.model('WebOverallRoleData', webOverallRoleData);
